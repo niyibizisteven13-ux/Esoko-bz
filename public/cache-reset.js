df43085d119756746db940e02bf14b6d0860b18c
stevy304 <niyibizi00003@gmail.com>
2026-07-31 18:49:07 +0200

    Move cache reset outside inline CSP

---
+++ public/cache-reset.js
@@ -0,0 +1,24 @@
+// Remove caches created by older service workers that could return stale
+// hashed chunks after a deployment. This runs once per browser session.
+(() => {
+  const resetKey = 'bwenge-service-worker-reset-v1';
+  try {
+    if (sessionStorage.getItem(resetKey)) return;
+    sessionStorage.setItem(resetKey, '1');
+  } catch {
+    return;
+  }
+
+  if (!('serviceWorker' in navigator)) return;
+
+  Promise.all([
+    navigator.serviceWorker.getRegistrations().then((registrations) =>
+      Promise.all(registrations.map((registration) => registration.unregister()))
+    ),
+    'caches' in window
+      ? caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+      : Promise.resolve(),
+  ])
+    .then(() => window.location.reload())
+    .catch(() => undefined);
+})();
